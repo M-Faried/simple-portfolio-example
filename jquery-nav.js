9deed1e1e54f29382fea4767145c0fec9cda2724
M Faried <m.a.faried@gmail.com>
2020-04-09 02:02:26 +0200

    Updates

--- jquery-nav.js
+++ jquery-nav.js
@@ -1,6 +1,6 @@
 // The short cur for jQuery is $
-jQuery(document).ready(function() {
-  jQuery('.burger-menu').on('click', function() {
+$(document).ready(function () {
+  $('.burger-menu').on('click', function () {
     // $('.mobile-nav').toggle('slow');
     // $('.mobile-nav').toggle(500);
     $('.mobile-nav').fadeToggle(500);
