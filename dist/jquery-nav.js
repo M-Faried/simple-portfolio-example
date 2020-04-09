844af4d0b602ba8a3144a0c65001389cb9835b23
M Faried <m.a.faried@gmail.com>
2020-04-09 02:01:48 +0200

    Moving scripts to the head of the document

--- dist/jquery-nav.js
+++ dist/jquery-nav.js
@@ -1,8 +1,8 @@
-// // The short cur for jQuery is $
-// jQuery(document).ready(function() {
-//   jQuery('.burger-menu').on('click', function() {
-//     // $('.mobile-nav').toggle('slow');
-//     // $('.mobile-nav').toggle(500);
-//     $('.mobile-nav').fadeToggle(500);
-//   });
-// });
+// The short cur for jQuery is $
+$(document).ready(function () {
+  $('.burger-menu').on('click', function () {
+    // $('.mobile-nav').toggle('slow');
+    // $('.mobile-nav').toggle(500);
+    $('.mobile-nav').fadeToggle(500);
+  });
+});
